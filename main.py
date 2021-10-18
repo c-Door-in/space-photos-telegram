95f4baf469e86147b2195c742a390586e38581dc
c_door In <55236660+git-tutorial-cmd@users.noreply.github.com>
2021-10-18 17:35:03 +0300

    Add download_image() and create main() and 'if __name__'

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ import os
 
 os.makedirs('images', exist_ok=True)
 
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-response = requests.get(url)
+def download_image(url, image_name):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(f'images/{image_name}', 'wb') as file:
+        file.write(response.content)
 
-with open('images/hubble.jpeg', 'wb') as file:
-    file.write(response.content)
+
+def main():
+    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    name = 'hubble.jpeg'
+    download_image(url, name)
+
+
+if __name__ == '__main__':
+    main()
