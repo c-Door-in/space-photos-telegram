6b1e9ca4fcdb801929f5d53b6d8600ece9de5c3f
c_door In <55236660+git-tutorial-cmd@users.noreply.github.com>
2021-10-18 17:21:45 +0300

    Add 'image' safety creating

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import requests
+import os
+
+
+os.makedirs('images', exist_ok=True)
 
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 response = requests.get(url)
 
-with open('files/hubble.jpeg', 'wb') as file:
+with open('images/hubble.jpeg', 'wb') as file:
     file.write(response.content)
