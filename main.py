22d4687da8ed4053890762a005d965cf1a0a6d5b
c_door In <55236660+git-tutorial-cmd@users.noreply.github.com>
2021-10-19 18:53:21 +0300

    Add parse file extension function

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 import os
+from urllib.parse import urlsplit, unquote
 
-
-os.makedirs('images', exist_ok=True)
 
 def download_image(url, image_name):
     response = requests.get(url)
@@ -22,11 +21,19 @@ def get_spacex_images_path(flight_id):
 def fetch_spacex_one_launch():
     flight_id = '5fe3b11eb3467846b324216c'
     for image_id, spacex_image_url in enumerate(get_spacex_images_path(flight_id)):
-        image_name = f'space{image_id}.jpeg'
+        ext = parse_url_file_ext(spacex_image_url)
+        image_name = f'space{image_id}{ext}'
         download_image(spacex_image_url, image_name)
 
 
+def parse_url_file_ext(url):
+    path = unquote(urlsplit(url).path)
+    filename = os.path.basename(path)
+    return os.path.splitext(filename)[1]
+
+
 def main():
+    os.makedirs('images', exist_ok=True)
     fetch_spacex_one_launch()
 
 
