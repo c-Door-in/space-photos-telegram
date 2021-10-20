af41d07b61fb43233dd4e80b1c46f92e922873b7
c_door In <55236660+git-tutorial-cmd@users.noreply.github.com>
2021-10-21 01:17:28 +0300

    Add public photo script

---
+++ public_photo.py
@@ -0,0 +1,24 @@
+import telegram
+import time
+from os import listdir
+
+from environs import Env
+
+
+def upload_photo(bot, chat_id, period):
+    for dir in listdir('images'):
+        for file in listdir(f'images/{dir}'):
+            bot.send_photo(chat_id=chat_id, photo=open(f'images/{dir}/{file}', 'rb'))
+            time.sleep(period)
+
+
+def main():
+    env = Env()
+    env.read_env()
+    bot = telegram.Bot(token=env('TG_TOKEN'))
+    chat_id = '@spimg'
+    upload_photo(bot, chat_id, env.int('PERIOD'))
+
+
+if __name__ == '__main__':
+    main()
